a0f0381d9d974af60f65b30c94fcefe7d166c1a7
206 <206@academicos.cesde.edu.co>
2022-11-21 12:22:39 -0500

    se crea base

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import org.example.clases.Jugador;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner entradaPorTeclado=new Scanner(System.in);
+
+        ArrayList<Jugador> jugador=new ArrayList<Jugador>();
+
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/clases/Jugador.java
@@ -0,0 +1,21 @@
+package org.example.clases;
+
+public class Jugador {
+
+    public String nombre;
+    public int edad;
+    public String apellido;
+    public String posicion;
+    public String equipo;
+    public int numeroDeLaCamiza;
+
+
+
+
+
+
+
+
+
+
+}
